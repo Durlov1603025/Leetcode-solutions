6fd021a721107622fdd1b9c7e581f2b30942640c
Durlov1603025 <53161692+Durlov1603025@users.noreply.github.com>
2022-12-18 19:47:36 +0600

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,8 +6,6 @@ public:
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if (nums[i] + nums[j] == target){
-                    //result[0] = i;
-                    //result[1] = j;
                     result.push_back(i);
                     result.push_back(j);
                 }
