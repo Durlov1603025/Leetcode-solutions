79943a2afd1cc4e8815322e4eab89a230699001d
Durlov1603025 <53161692+Durlov1603025@users.noreply.github.com>
2022-12-29 20:24:14 +0600

    Time: 0 ms (100.00%), Space: 6 MB (36.36%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0) return false;
+        while(n%4==0)
+        {
+            n=n/4;
+        }
+        if(n!=1){
+        return false;}
+        return true;
+    }
+};
