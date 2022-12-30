75d860a7ecf9e6150c976e4b55e9a175376ec88e
Durlov1603025 <53161692+Durlov1603025@users.noreply.github.com>
2022-12-30 23:35:05 +0600

    Time: 3 ms (42.44%), Space: 6 MB (88.79%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int n= s.size();
+        int i=0, j=n-1;
+        while(i<j)
+        {
+            if(isalpha(s[i]) && isalpha(s[j]) )
+            {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+            else if(!isalpha(s[i]) ) {i++;}
+            else if(!isalpha(s[j]) ) {j--;}
+            
+        }
+        return s;
+    }
+};
